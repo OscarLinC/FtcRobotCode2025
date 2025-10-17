2a60ff075d46b479e88451539a6f4c7689fddac7
Adam's Computer <yenchu26@kas.tw>
2025-10-17 18:12:36 +0800

    This is a test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test1.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+
+@TeleOp
+public class Test1 extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        while (opModeIsActive()) {
+
+        }
+    }
+}
